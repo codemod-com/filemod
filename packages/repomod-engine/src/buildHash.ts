d35f2fa58c5f79b15699f3560ccfc18d8a7872cb
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-03-28 13:17:21 +0200

    wip

--- packages/repomod-engine/src/buildHash.ts
+++ packages/repomod-engine/src/buildHash.ts
@@ -1,4 +1,4 @@
 import { createHash } from 'crypto';
 
-export const buildHash = (data: string) =>
+export const buildHashDigest = (data: string) =>
 	createHash('ripemd160').update(data).digest('base64url');
--- packages/repomod-engine/src/files.ts
+++ packages/repomod-engine/src/files.ts
@@ -1,5 +1,6 @@
 import * as fs from 'node:fs';
 import * as platformPath from 'node:path';
+import { buildHashDigest } from './buildHash';
 
 interface FacadeFile {
 	readonly kind: 'file';
@@ -14,9 +15,18 @@ interface FacadeDirectory {
 }
 
 type FacadeEntry = FacadeFile | FacadeDirectory;
+type FacadeEntryHashDigest = string & {
+	__FacadeEntryHashDigest: '__FacadeEntryHashDigest';
+};
+
+const buildFacadeEntryHashDigest = (
+	facadeEntry: FacadeEntry,
+): FacadeEntryHashDigest => {
+	return buildHashDigest(facadeEntry.path) as FacadeEntryHashDigest;
+};
 
 export class FacadeFileSystem {
-	private __facadeEntries: FacadeEntry[] = []; // TODO to be optimized
+	private __facadeEntries = new Map<FacadeEntryHashDigest, FacadeEntry>();
 	private __readDirectories = new Set<string>();
 
 	public constructor(private __realFileSystem: typeof fs) {}
@@ -45,19 +55,27 @@ export class FacadeFileSystem {
 				const entryPath = platformPath.join(path, entry.name);
 
 				if (entry.isDirectory()) {
-					this.__facadeEntries.push({
+					const facadeEntry: FacadeEntry = {
 						kind: 'directory',
 						path: entryPath,
 						level,
-					});
+					};
+
+					const hashDigest = buildFacadeEntryHashDigest(facadeEntry);
+
+					this.__facadeEntries.set(hashDigest, facadeEntry);
 				}
 
 				if (entry.isFile()) {
-					this.__facadeEntries.push({
+					const facadeEntry: FacadeEntry = {
 						kind: 'file',
 						path: entryPath,
 						level,
-					});
+					};
+
+					const hashDigest = buildFacadeEntryHashDigest(facadeEntry);
+
+					this.__facadeEntries.set(hashDigest, facadeEntry);
 				}
 			});
 		}
