c1f3b6fa8943a8c781c2d444706bdbbdef555cf6
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-03-28 12:10:50 +0200

    wip

--- packages/repomod-engine/src/index.ts
+++ packages/repomod-engine/src/index.ts
@@ -1,61 +1,61 @@
-import * as fs from "node:fs";
-import * as fsPromises from "node:fs/promises";
+import * as fs from 'node:fs';
+import * as fsPromises from 'node:fs/promises';
 
 type Options = Readonly<Record<string, string | undefined>>;
 
 export interface UpsertFileCommand {
-  readonly kind: "upsertFile";
-  readonly path: string;
-  readonly options: Options;
+	readonly kind: 'upsertFile';
+	readonly path: string;
+	readonly options: Options;
 }
 
 export interface DeleteFileCommand {
-  readonly kind: "deleteFile";
-  readonly path: string;
-  readonly options: Options;
+	readonly kind: 'deleteFile';
+	readonly path: string;
+	readonly options: Options;
 }
 
 export interface MoveFileCommand {
-  readonly kind: "moveFile";
-  readonly oldPath: string;
-  readonly newPath: string;
-  readonly options: Options;
+	readonly kind: 'moveFile';
+	readonly oldPath: string;
+	readonly newPath: string;
+	readonly options: Options;
 }
 
 export interface CopyFileCommand {
-  readonly kind: "copyFile";
-  readonly oldPath: string;
-  readonly newPath: string;
-  readonly options: Options;
+	readonly kind: 'copyFile';
+	readonly oldPath: string;
+	readonly newPath: string;
+	readonly options: Options;
 }
 
 export type FileCommand =
-  | UpsertFileCommand
-  | DeleteFileCommand
-  | MoveFileCommand
-  | CopyFileCommand;
+	| UpsertFileCommand
+	| DeleteFileCommand
+	| MoveFileCommand
+	| CopyFileCommand;
 
 export interface HandleDirectoryCommand {
-  readonly kind: "handleDirectory";
-  readonly path: string;
-  readonly options: Options;
+	readonly kind: 'handleDirectory';
+	readonly path: string;
+	readonly options: Options;
 }
 
 export interface HandleFileCommand {
-  readonly kind: "handleFile";
-  readonly path: string;
-  readonly options: Options;
+	readonly kind: 'handleFile';
+	readonly path: string;
+	readonly options: Options;
 }
 
 export type DirectoryCommand = HandleDirectoryCommand | HandleFileCommand;
 
 export interface ExportDataCommand {
-  readonly kind: "upsertData";
-  readonly data: string;
+	readonly kind: 'upsertData';
+	readonly data: string;
 }
 
 export interface NoopCommand {
-  readonly kind: "noop";
+	readonly kind: 'noop';
 }
 
 export type DataCommand = ExportDataCommand | NoopCommand;
@@ -63,90 +63,90 @@ export type DataCommand = ExportDataCommand | NoopCommand;
 export type Command = DirectoryCommand | FileCommand | DataCommand;
 
 export interface PathAPI {
-  readonly getDirname: (path: string) => string; // might throw
-  readonly getBasename: (path: string) => string; // might throw
-  readonly joinPaths: (...paths: string[]) => string; // might throw
+	readonly getDirname: (path: string) => string; // might throw
+	readonly getBasename: (path: string) => string; // might throw
+	readonly joinPaths: (...paths: string[]) => string; // might throw
 }
 
 interface DataAPI extends PathAPI {
-  getDependencies: () => Record<string, unknown>;
+	getDependencies: () => Record<string, unknown>;
 }
 
 interface FileAPI extends PathAPI, DataAPI {
-  // patterns and paths
-  readonly includePatterns: ReadonlyArray<string>;
-  readonly excludePatterns: ReadonlyArray<string>;
+	// patterns and paths
+	readonly includePatterns: ReadonlyArray<string>;
+	readonly excludePatterns: ReadonlyArray<string>;
 
-  readonly isDirectory: (path: string) => boolean; // might throw
-  readonly exists: (path: string) => Promise<boolean>;
+	readonly isDirectory: (path: string) => boolean; // might throw
+	readonly exists: (path: string) => Promise<boolean>;
 
-  // reading directories and files
-  readonly readFile: (filePath: string) => Promise<string>; // might throw
+	// reading directories and files
+	readonly readFile: (filePath: string) => Promise<string>; // might throw
 }
 
 interface DirectoryAPI extends FileAPI {
-  readonly readDirectory: (
-    directoryPath: string
-  ) => Promise<ReadonlyArray<string>>; // might throw
-  readonly getFilePaths: (
-    directoryPath: string,
-    includePatterns: ReadonlyArray<string>,
-    excludePatterns: ReadonlyArray<string>
-  ) => Promise<ReadonlyArray<string>>;
+	readonly readDirectory: (
+		directoryPath: string,
+	) => Promise<ReadonlyArray<string>>; // might throw
+	readonly getFilePaths: (
+		directoryPath: string,
+		includePatterns: ReadonlyArray<string>,
+		excludePatterns: ReadonlyArray<string>,
+	) => Promise<ReadonlyArray<string>>;
 }
 
 export interface Repomod {
-  readonly handleDirectory?: (
-    api: DirectoryAPI,
-    path: string,
-    options: Options
-  ) => Promise<ReadonlyArray<DirectoryCommand>>;
-  readonly handleFile?: (
-    api: FileAPI,
-    path: string,
-    options: Options
-  ) => Promise<ReadonlyArray<FileCommand>>;
-  readonly handleData?: (
-    api: DataAPI,
-    path: string,
-    data: string,
-    options: Options
-  ) => Promise<DataCommand>;
+	readonly handleDirectory?: (
+		api: DirectoryAPI,
+		path: string,
+		options: Options,
+	) => Promise<ReadonlyArray<DirectoryCommand>>;
+	readonly handleFile?: (
+		api: FileAPI,
+		path: string,
+		options: Options,
+	) => Promise<ReadonlyArray<FileCommand>>;
+	readonly handleData?: (
+		api: DataAPI,
+		path: string,
+		data: string,
+		options: Options,
+	) => Promise<DataCommand>;
 }
 
 export interface API {
-  fileSystem: typeof fs;
-  promisifiedFileSystem: typeof fsPromises;
-  directoryAPI: DirectoryAPI;
-  fileAPI: FileAPI;
+	fileSystem: typeof fs;
+	promisifiedFileSystem: typeof fsPromises;
+	directoryAPI: DirectoryAPI;
+	fileAPI: FileAPI;
 }
 
-const defaultHandleDirectory: Repomod["handleDirectory"] = async (
-  api,
-  directoryPath,
-  options
+const defaultHandleDirectory: Repomod['handleDirectory'] = async (
+	api,
+	directoryPath,
+	options,
 ) => {
-  const filePaths = await api.getFilePaths(
-    directoryPath,
-    api.includePatterns,
-    api.excludePatterns
-  );
+	const filePaths = await api.getFilePaths(
+		directoryPath,
+		api.includePatterns,
+		api.excludePatterns,
+	);
 
-  return filePaths.map((path) => ({
-    kind: "handleFile",
-    path,
-    options,
-  }));
+	return filePaths.map((path) => ({
+		kind: 'handleFile',
+		path,
+		options,
+	}));
 };
 
-const defaultHandleFile: Repomod["handleFile"] = async (_, path, options) => {
-  return [
-    {
-      kind: "upsertFile",
-      path,
-      options,
-    },
-  ];
+const defaultHandleFile: Repomod['handleFile'] = async (_, path, options) => {
+	return [
+		{
+			kind: 'upsertFile',
+			path,
+			options,
+		},
+	];
 };
 
 // const handleDirectoryCommand = async (
@@ -190,64 +190,73 @@ const defaultHandleFile: Repomod["handleFile"] = async (_, path, options) => {
 // };
 
 const handleCommand = async (
-  api: API,
-  repomod: Repomod,
-  command: Command
+	api: API,
+	repomod: Repomod,
+	command: Command,
 ): Promise<void> => {
-  if (command.kind === "handleDirectory") {
-    const stat = api.fileSystem.statSync(command.path, {
-      throwIfNoEntry: false,
-    });
+	if (command.kind === 'handleDirectory') {
+		const stat = api.fileSystem.statSync(command.path, {
+			throwIfNoEntry: false,
+		});
 
-    if (stat?.isDirectory()) {
-      const handleDirectory = repomod.handleDirectory ?? defaultHandleDirectory;
+		if (stat?.isDirectory()) {
+			const handleDirectory =
+				repomod.handleDirectory ?? defaultHandleDirectory;
 
-      const commands = await handleDirectory(
-        api.directoryAPI,
-        command.path,
-        command.options
-      );
-    }
-  }
+			const commands = await handleDirectory(
+				api.directoryAPI,
+				command.path,
+				command.options,
+			);
 
-  if (command.kind === "handleFile") {
-    const stat = api.fileSystem.statSync(command.path, {
-      throwIfNoEntry: false,
-    });
+			for (const command of commands) {
+				await handleCommand(api, repomod, command);
+			}
+		}
+	}
 
-    if (stat?.isFile()) {
-      const handleFile = repomod.handleFile ?? defaultHandleFile;
+	if (command.kind === 'handleFile') {
+		const stat = api.fileSystem.statSync(command.path, {
+			throwIfNoEntry: false,
+		});
 
-      const commands = await handleFile(
-        api.fileAPI,
-        command.path,
-        command.options
-      );
-    }
-  }
+		if (stat?.isFile()) {
+			const handleFile = repomod.handleFile ?? defaultHandleFile;
+
+			const commands = await handleFile(
+				api.fileAPI,
+				command.path,
+				command.options,
+			);
+
+			for (const command of commands) {
+				await handleCommand(api, repomod, command);
+			}
+		}
+	}
 };
 
 export const executeRepomod = async (
-  api: API,
-  repomod: Repomod,
-  path: string,
-  options: Options
+	api: API,
+	repomod: Repomod,
+	path: string,
+	options: Options,
 ) => {
-  const stat = api.fileSystem.statSync(path, {
-    throwIfNoEntry: false,
-  });
+	const stat = api.fileSystem.statSync(path, {
+		throwIfNoEntry: false,
+	});
 
-  if (stat === undefined || (!stat.isDirectory() && !stat.isFile())) {
-    return [];
-  }
+	if (stat === undefined || (!stat.isDirectory() && !stat.isFile())) {
+		return [];
+	}
 
-  const command: DirectoryCommand = {
-    kind: stat.isDirectory() ? "handleDirectory" : "handleFile",
-    path,
-    options,
-  };
+	const command: DirectoryCommand = {
+		kind: stat.isDirectory() ? 'handleDirectory' : 'handleFile',
+		path,
+		options,
+	};
 
-  await handleCommand(api, repomod, command);
+	await handleCommand(api, repomod, command);
 
-  // return api.getFiles();
+	// return api.getFiles();
 };
