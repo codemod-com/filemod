e41e166b38ee5a878d87fb5338aeaaeb9ff7fae6
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-03-29 16:47:10 +0200

    wip

--- packages/repomod-engine-api/src/index.ts
+++ packages/repomod-engine-api/src/index.ts
@@ -1,93 +1,6 @@
-import * as platformPath from 'node:path';
 import { ExternalFileCommand } from './externalFileCommands.js';
 import { UnifiedFileSystem } from './unifiedFileSystem.js';
 import { FileSystemManager } from './fileSystemManager.js';
-
-type RSU = Record<string, unknown>;
-
-type Options = Readonly<Record<string, string | undefined>>;
-
-export interface UpsertFileCommand {
-	readonly kind: 'upsertFile';
-	readonly path: string;
-	readonly options: Options;
-}
-
-export interface DeleteFileCommand {
-	readonly kind: 'deleteFile';
-	readonly path: string;
-}
-
-export interface MoveFileCommand {
-	readonly kind: 'moveFile';
-	readonly oldPath: string;
-	readonly newPath: string;
-	readonly options: Options;
-}
-
-export interface CopyFileCommand {
-	readonly kind: 'copyFile';
-	readonly oldPath: string;
-	readonly newPath: string;
-	readonly options: Options;
-}
-
-export type FileCommand =
-	| UpsertFileCommand
-	| DeleteFileCommand
-	| MoveFileCommand
-	| CopyFileCommand;
-
-export interface HandleDirectoryCommand {
-	readonly kind: 'handleDirectory';
-	readonly path: string;
-	readonly options: Options;
-}
-
-export interface HandleFileCommand {
-	readonly kind: 'handleFile';
-	readonly path: string;
-	readonly options: Options;
-}
-
-export type DirectoryCommand = HandleDirectoryCommand | HandleFileCommand;
-
-export interface UpsertDataCommand {
-	readonly kind: 'upsertData';
-	readonly data: string;
-	readonly path: string; // TODO we can remove it and add from context at a later stageW
-}
-
-export interface NoopCommand {
-	readonly kind: 'noop';
-}
-
-export type DataCommand = UpsertDataCommand | NoopCommand;
-
-export type Command = DirectoryCommand | FileCommand | DataCommand;
-
-export interface PathAPI {
-	readonly getDirname: (path: string) => string; // might throw
-	readonly getBasename: (path: string) => string; // might throw
-	readonly joinPaths: (...paths: string[]) => string; // might throw
-}
-
-export interface DataAPI<D extends RSU> extends PathAPI {
-	getDependencies: () => D;
-}
-
-export interface FileAPI<D extends RSU> extends PathAPI, DataAPI<D> {
-	readonly isDirectory: (path: string) => boolean;
-	readonly exists: (path: string) => boolean;
-	// reading directories and files
-	readonly readFile: (filePath: string) => Promise<string>;
-}
-
-export interface DirectoryAPI<D extends RSU> extends FileAPI<D> {
-	readonly readDirectory: (
-		directoryPath: string,
-	) => Promise<readonly string[]>; // might throw
-}
 
 export interface Repomod<D extends RSU> {
 	readonly includePatterns?: readonly string[];
@@ -108,13 +21,6 @@ export interface Repomod<D extends RSU> {
 		data: string,
 		options: Options,
 	) => Promise<DataCommand>;
-}
-
-export interface API<D extends RSU> {
-	unifiedFileSystem: UnifiedFileSystem;
-	directoryAPI: DirectoryAPI<D>;
-	fileAPI: FileAPI<D>;
-	dataAPI: DataAPI<D>;
 }
 
 // eslint-disable-next-line @typescript-eslint/ban-types
@@ -254,41 +160,6 @@ const handleCommand = async <D extends RSU>(
 	if (command.kind === 'upsertData') {
 		api.unifiedFileSystem.upsertData(command.path, command.data);
 	}
-};
-
-export const buildApi = <D extends RSU>(
-	unifiedFileSystem: UnifiedFileSystem,
-	getDependencies: DataAPI<D>['getDependencies'],
-): API<D> => {
-	const pathAPI: PathAPI = {
-		getDirname: (path) => platformPath.dirname(path),
-		getBasename: (path) => platformPath.basename(path),
-		joinPaths: (...paths) => platformPath.join(...paths),
-	};
-
-	const dataAPI: DataAPI<D> = {
-		getDependencies,
-		...pathAPI,
-	};
-
-	const directoryAPI: DirectoryAPI<D> = {
-		readDirectory: (path) => unifiedFileSystem.readDirectory(path),
-		isDirectory: (path) => unifiedFileSystem.isDirectory(path),
-		exists: (path) => unifiedFileSystem.exists(path),
-		readFile: (path) => unifiedFileSystem.readFile(path),
-		...dataAPI,
-	};
-
-	const fileAPI: FileAPI<D> = {
-		...directoryAPI,
-	};
-
-	return {
-		directoryAPI,
-		unifiedFileSystem,
-		fileAPI,
-		dataAPI,
-	};
 };
 
 export const executeRepomod = async <D extends RSU>(
