0313809d58939e2d60f51e47e7c96b4d2239bc9a
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-06-07 19:25:38 +0200

    INT-1080 add mocha support

--- packages/repomod-engine-api/src/unifiedFileSystem.ts
+++ packages/repomod-engine-api/src/unifiedFileSystem.ts
@@ -209,18 +209,12 @@ export class UnifiedFileSystem {
 		includePatterns: readonly string[],
 		excludePatterns: readonly string[],
 	): Promise<readonly string[]> {
-		const twoDimentionalPaths = await Promise.all(
-			includePatterns.map((includePattern) =>
-				glob(includePattern, {
-					absolute: true,
-					cwd: directoryPath,
-					fs: this.__realFileSystem,
-					ignore: excludePatterns.slice(),
-				}),
-			),
-		);
-
-		const paths = new Set(twoDimentionalPaths.flat());
+		const paths = await glob(includePatterns.slice(), {
+			absolute: true,
+			cwd: directoryPath,
+			fs: this.__realFileSystem,
+			ignore: excludePatterns.slice(),
+		});
 
 		paths.forEach((path) => {
 			const unifiedFile: UnifiedFile = {
@@ -233,7 +227,7 @@ export class UnifiedFileSystem {
 			this.__entries.set(pathHashDigest, unifiedFile);
 		});
 
-		return Array.from(paths);
+		return paths;
 	}
 
 	public deleteFile(filePath: string): void {
